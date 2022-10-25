007ebece448d40ec7ab0602f2de3dd187ff64b72
Dhruv Gambhir <75164918+dhruv-gambhir@users.noreply.github.com>
2022-10-26 00:34:14 +0800

    Update AgeCategory.java

--- MOBLIMA application/src/agepackage/AgeCategory.java
+++ MOBLIMA application/src/agepackage/AgeCategory.java
@@ -1,6 +1,20 @@
 package agepackage;
 public enum AgeCategory {
-	SeniorCitizen,
-	Adult,
-	Child
+	SENIOR("Senior Citizen"),
+	ADULT("Adult"),
+	CHILD("Child');
+	      
+	private String category;
+	      
+	AgeCategory(String category) {
+		this.category = category;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public boolean equals(String category) {
+		if (this.category==category)
+			return true;
+		return false;
+	}
 }
