37654ae0b33ab555dd3de6ae5fe05c6ba846224c
Dhruv Gambhir <75164918+dhruv-gambhir@users.noreply.github.com>
2022-10-26 15:21:08 +0800

    Update Age.java

--- MOBLIMA application/src/agepackage/Age.java
+++ MOBLIMA application/src/agepackage/Age.java
@@ -4,16 +4,21 @@ public class Age implements IAge {
 
 	
 	private int age = getAge();
-	private int multiplier;
 	private AgeCategory category;
 	      
-	public void setCategory(int age) {
-		if(age>=60)
+	public float getMultiplier(int age) {
+		if(age>=60){
 			this.category = AgeCategory.SENIOR;
-		else if(age<=12)
+			return 0.7;
+		}
+		else if(age<=12){
 			this.category = AgeCategory.CHILD;
-		else
+			return 0.5;
+		}
+		else{
 			this.category = AgeCategory.ADULT;
+			return 1;
+		}
 	}
 	
 	
