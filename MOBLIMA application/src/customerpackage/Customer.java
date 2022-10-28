96d1ed12c9191df8eeb8350cfa67d105433911a4
HiIAmTzeKean <ngtzekean600@gmail.com>
2022-10-28 21:13:21 +0800

    Customer package

--- MOBLIMA application/src/customerpackage/Customer.java
+++ MOBLIMA application/src/customerpackage/Customer.java
@@ -1,6 +1,5 @@
 package customerpackage;
-import agepackage.*;
-import showtimepackage.*;
+import agepackage.Age;
 
 public class Customer {
 
@@ -8,6 +7,38 @@ public class Customer {
 	private int mobile;
 	private String email;
 	private Age age;
-	private Showtime bookingHistory;
 
+	public Customer(String name, int mobile, String email, Age age){
+		this.name = name;
+		this.mobile = mobile;
+		this.email = email;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Age getAge() {
+		return age;
+	}
+	public void setAge(Age age) {
+		this.age = age;
+	}
+	public float getMultiplier(){
+		return age.getMultiplier();
+	}
 }
