1d84271947471c4194686217a694edfcc04714c5
Mishra-Apurva <100474366+Mishra-Apurva@users.noreply.github.com>
2022-10-28 16:32:49 +0800

    Update Customer.java

--- MOBLIMA application/src/customerpackage/Customer.java
+++ MOBLIMA application/src/customerpackage/Customer.java
@@ -1,13 +1,30 @@
 package customerpackage;
 import agepackage.*;
 import showtimepackage.*;
+import java.util.Scanner;
+
+//todo: implement booking history
 
 public class Customer {
-
+	public static Scanner scan = new Scanner(System.in);
 	private String name;
-	private int mobile;
+	private int age;
+	private long mobile;
 	private String email;
-	private Age age;
-	private Showtime bookingHistory;
-
+	
+	public void setCustomerDetails() {
+		System.out.println("Enter the following customer details:- ");
+		System.out.print("Enter name: ");
+		name = scan.next();
+		System.out.print("Enter age: ");
+		age = scan.nextInt();
+		System.out.print("Enter mobile: ");
+		mobile = scan.nextLong();
+		System.out.print("Enter email: ");
+		email = scan.next();
+	}
+	
+	public int getAge() {
+		return age;
+	}
 }
