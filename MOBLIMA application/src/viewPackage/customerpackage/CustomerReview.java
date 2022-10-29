168fdfb183c1d843bda9d56c08d97f88d2912e48
Mishra-Apurva <100474366+Mishra-Apurva@users.noreply.github.com>
2022-10-29 14:19:09 +0800

    Update CustomerReview.java

--- MOBLIMA application/src/viewPackage/customerpackage/CustomerReview.java
+++ MOBLIMA application/src/viewPackage/customerpackage/CustomerReview.java
@@ -1,4 +1,30 @@
-package viewPackage.customerpackage;
+package viewPackage.customerviewpackage;
 
+import java.util.Scanner;
+
+//just get and set for review and rating. 
+//Check if reviewpackage needs something else 
 public class CustomerReview {
+	private String review;
+	private int rating;
+	
+	public static Scanner scan = new Scanner(System.in);
+	
+	public void setReview() {
+		System.out.print("Please enter your review: ");
+		review = scan.next();
+	}
+	
+	public void setRating() {
+		System.out.print("Please enter your rating: ");
+		rating = scan.nextInt();
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	public String getReview() {
+		return review;
+	}
 }
