949d4d3d946145d49d1ec21daa59cd6ddb619081
Mishra-Apurva <100474366+Mishra-Apurva@users.noreply.github.com>
2022-10-31 23:06:38 +0800

    Update CustomerReview.java

--- MOBLIMA application/src/viewPackage/customerpackage/CustomerReview.java
+++ MOBLIMA application/src/viewPackage/customerpackage/CustomerReview.java
@@ -6,19 +6,18 @@ import moviepackage.IReviews;
 public class CustomerReview {
 	private String review;
 	private float rating;
-	//could this use a movie type object, which can be set using getMovieFromID()?
 	public static Scanner scan = new Scanner(System.in);
 	
-	//change to addReview() by movie name
-	public void setReviewAndRating(int movieID) {
+	public void setReviewAndRating(String movieName) {
 		System.out.print("Please enter your review: ");
 		review = scan.next();
 		System.out.print("Please enter your rating: ");
 		rating = scan.nextFloat();
 		IReviews ir; //check this part
-		ir.addReview(movieID, review, rating);
+		ir.addReview(movieName, review, rating);
 	}
 	
+	//check if needs interface for access by IReviews
 	public String getReview() {
 		return review;
 	}
