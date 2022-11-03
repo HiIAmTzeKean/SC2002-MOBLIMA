6faedb699728ef5b8a91e33644295cd57f9e4bd5
najanajah <104294671+najanajah@users.noreply.github.com>
2022-11-03 13:30:19 +0800

    Update MOBLIMA.java

--- MOBLIMA application/src/viewPackage/MOBLIMA.java
+++ MOBLIMA application/src/viewPackage/MOBLIMA.java
@@ -7,10 +7,13 @@ public class MOBLIMA extends View {
     }
 
     public static void main(){ 
+        System.out.println("----------------------------------");
         System.out.println("\t\t\tWELCOME TO MOBLIMA "); 
         System.out.println("----------------------------------");
 
         start(); 
+        
+        System.out.println("----------------------------------");
         System.out.println("\t\t\tEXITING MOBLIMA "); 
         System.out.println("----------------------------------");
     }
