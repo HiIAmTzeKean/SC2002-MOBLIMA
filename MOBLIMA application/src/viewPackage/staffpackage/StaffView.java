b93cd64c85702906664e84688d2ba35e9686bffd
najanajah <104294671+najanajah@users.noreply.github.com>
2022-11-03 13:31:11 +0800

    Update StaffView.java

--- MOBLIMA application/src/viewPackage/staffpackage/StaffView.java
+++ MOBLIMA application/src/viewPackage/staffpackage/StaffView.java
@@ -1,63 +1,55 @@
-package viewPackage.staffpackage;
-
-import viewPackage.View;
-
+package viewPackage;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-
-public class StaffView extends View {
-
-    public static void displayMenu(){ 
-        System.out.println("\t\t\tStaff Main Menu");
-		System.out.println("---------------------------------------------------");
-		System.out.println("Choice 1 : Create/Update/Remove Movie listing");
-		System.out.println("Choice 2 : Create/Update/Remove Cinemas");
-		System.out.println("Choice 3 : Create/Update/Remove Showtimes");
-		System.out.println("Choice 4 : Configure System Settings");
-		System.out.println("Choice 5 : logout");
-        System.out.println("---------------------------------------------------");
-    } 
+import viewPackage.customerpackage.CustomerView;
 
 
+public class View {
 
-    public static void start(){
+	public static void displayMenu() {
+		// System.out.println("WELCOME TO MOBLIMA");
+		System.out.println("--------------------------------------");
+		System.out.println("\t\tMenu\t\t");
+		System.out.println("--------------------------------------");
+		System.out.println("Choice 1 : Staff");
+		System.out.println("Choice 2 : Customer"); 
+		System.out.println("choice 3 : exit MOBLIMA"); 
+		System.out.println("--------------------------------------");
+	}
+
+	public static void start() throws InputMismatchException {
+		// TODO - implement View.start
 		
-        Scanner sc = new Scanner(System.in); 
-        int choice = 0 ; 
 		
-		 do {	
-			displayMenu();
-            try {
-                System.out.println("Enter choice"); 
-                choice = sc.nextInt();
-            } catch(InputMismatchException e ){ 
-                System.out.println(e.toString());
-            } 
-			//clearing the console
-			System.out.print("\033[H\033[2J");
+		Scanner sc = new Scanner(System.in);
+		int choice = 0; 
+
+		try { 
+			do { 
+				displayMenu(); 
+				System.out.println("\nEnter choice : ");
+
 			
-			switch (choice) { 
-				case  1 : StaffMovie.start(); 
-				break;
-				case  2 : StaffCinema.start(); 
-				break;
-				case  3 : StaffShowtime.start(); 
-				break;
-				case  4 : StaffSystem.start(); 
-				break;
-				case  5 : StaffAuth.logout();
-				break;
-				default : System.out.println("Enter valid choice");
-						  choice = 0;		
-			}
+				choice = sc.nextInt(); 
 
-		}while(choice<6 && choice>=0);
-        
-		System.out.println("\f");
-        sc.close(); 
+				switch(choice) { 
+				case 1: viewPackage.staffpackage.StaffAuth.login(); break; 
+				case 2: CustomerView.start(); break; 
+				case 3: return; 
+				default : System.out.println("Enter valid choice");
+				choice = 0;
+
+				}
+			} while (choice>=0 && choice <=3);
+		}catch( NoSuchElementException e){
+			System.out.println(e.toString());
+			sc.nextLine();
+			View.start();
+		}
+		
+		sc.close();
 	}
 
 }
-    
-
