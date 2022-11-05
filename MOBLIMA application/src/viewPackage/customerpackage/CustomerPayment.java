e247cca3fe322304a62fa7eb47f8b53fff6d0b54
Mishra-Apurva <100474366+Mishra-Apurva@users.noreply.github.com>
2022-11-05 20:04:54 +0800

    Update CustomerPayment.java

--- MOBLIMA application/src/viewPackage/customerpackage/CustomerPayment.java
+++ MOBLIMA application/src/viewPackage/customerpackage/CustomerPayment.java
@@ -1,10 +1,48 @@
 package viewPackage.customerpackage;
+import java.util.*;
+
+import agepackage.Age;
+import agepackage.IAge;
+import customerpackage.Customer;
 
 public class CustomerPayment {
-
-	public void getCustomerDetails() {
-		// TODO - implement CustomerPayment.getCustomerDetails
-		throw new UnsupportedOperationException();
+	private static Scanner scan = new Scanner(System.in);
+	private Customer c = null;
+	int customerID = 0;
+	
+	public void setCustomerDetails() {
+		String name = null;
+		IAge age = null;
+		int mobile = 0;
+		String email = null;
+		
+		System.out.println("\nPlease enter your personal details: ");
+		System.out.print("Name :");
+		name = scan.next();
+		c.setName(name);
+		System.out.println();
+		
+		System.out.print("age :");
+		int ageInt = scan.nextInt();
+		age.setAgeNumber(ageInt);
+		c.setAge((Age)age);
+		System.out.println();
+		
+		System.out.print("Mobile :");
+		mobile = scan.nextInt();
+		c.setMobile(mobile);
+		System.out.println();
+		
+		System.out.print("Email :");
+		email = scan.next();
+		c.setEmail(email);
+		System.out.println();
+		
+		customerID = c.getID();
+	}
+	
+	public int getCustomerID() {
+		return customerID;
 	}
 
 }
