a987409aaa826022bef589d900f631d1e89908c4
Mishra-Apurva <100474366+Mishra-Apurva@users.noreply.github.com>
2022-11-07 00:56:29 +0800

    Update CustomerShowtime.java

--- MOBLIMA application/src/viewPackage/customerpackage/CustomerShowtime.java
+++ MOBLIMA application/src/viewPackage/customerpackage/CustomerShowtime.java
@@ -55,53 +55,43 @@ public class CustomerShowtime {
 		return showtimeID;
 	}
 	
+	
+	//returns cineplexID or -1 in case of an error
+	public int chooseCineplex() {
+		ICineplex cineplexHandler = CineplexManager.getInstance();
+		System.out.println("The Cineplexes are:");
+		cineplexHandler.printCineplexes();
+		System.out.println();
+		System.out.println("Enter the name of the cineplex you have chosen:");
+		try {
+			selectedCineplexName = scan.nextLine();
+		}
+		catch(InputMismatchException e) {
+			selectedCineplexID = 0; //reset value
+			return -1;
+		}
+		System.out.println();
+		
+		try {
+			selectedCineplexObj = cineplexHandler.getCineplex(selectedCineplexName); 
+		}
+		catch(IllegalArgumentException e) {
+			selectedCineplexID = 0; //reset value
+			return -1;
+		}
+		
+		selectedCineplexID = selectedCineplexObj.getID();
+		return selectedCineplexID;
+	}
+	
 	/* 
 	 *display menu to interact with the cineplexes and movies available, and 
 	 *view all possible showtime for a combination.
+	 *returns true if the display of showtimes accepter 
+	 *and false if the 
 	 */
-	public void displayShowtimes() {
-
-		ICineplex cineplexHandler = CineplexManager.getInstance();
-		IMovie movieFindHandler = MovieManager.getInstance(); 
-		IShowtime showtimeHandler = ShowtimeManager.getInstance(); 
-		
-		boolean showtime_selection_contd = true;
-		//loop until a user selects a cineplex and movie combination for which non-zero showtimes exist.
-		do {
-			//STEP A - CHOOSE CINEPLEX
-			System.out.println("The Cineplexes are:");
-			cineplexHandler.printCineplexes();
-			System.out.println();
-			System.out.println("Enter the name of the cineplex you have chosen:");
-			selectedCineplexName = scan.next();
-			System.out.println();
-			try {
-				selectedCineplexObj = cineplexHandler.getCineplex(selectedCineplexName); 
-			}
-			catch(IllegalArgumentException ecineplex) {
-				System.out.println("Cineplex is not found — please try again");
-				continue; //to start of outer do-while loop
-			}
-			selectedCineplexID = selectedCineplexObj.getID();
-			
-			/////////////////////////////////////////////////////////////////////////////////////////////////
-			
-			//STEP B - CHOOSE MOVIE
-			CustomerMovieListing movSelectObj = new CustomerMovieListing();
-			movSelectObj.movieSelection(); //runs the movie selection menu
-			selectedMovieName = movSelectObj.getSelectedMovieName();
-			try {
-				selectedMovieObj =  movieFindHandler.findMoviebyName(selectedMovieName); 
-				selectedMovieObj.printMovieComplete();
-			}
-			catch(IllegalArgumentException e) {
-				System.out.println("Movie is not found — please try again");
-				continue; //to start of outer do-while loop
-			}
-			
-			/////////////////////////////////////////////////////////////////////////////////////////////////
-			
-			//STEP C - Display showtimes for the movie and cineplex combination (atleast one showtime should exist)
+	public boolean displayShowtimes() {
+		IShowtime showtimeHandler = ShowtimeManager.getInstance(); 	
 			
 			try {
 				System.out.printf("\nFollowing are the showtimes available for movie %s at cineplex %s:\n", selectedMovieName, selectedCineplexName);
@@ -110,9 +100,8 @@ public class CustomerShowtime {
 			}
 			catch(IllegalArgumentException e) {
 				System.out.println("No Showtimes exist for the Movie and Cineplex combination — please try again");
-				continue; //to start of outer do-while loop
+				return false;
 			}
-			
 			/*
 			 * If user does not like any of the showtimes available, 
 			 * allow to re-enter and movie and cinplex combination
@@ -122,103 +111,93 @@ public class CustomerShowtime {
 			System.out.println("To pick cineplex and movie again, for another set of showtimes, enter 0. Else enter any other character:");
 			String re_enter = scan.next();
 			if(re_enter.compareTo("0") == 0) {
-				continue; //to start of outer do-while loop
+				return false;
 			}
-			
-		}while(showtime_selection_contd);
-		
-		CineplexManager.close();
-		MovieManager.close();
-		ShowtimeManager.close();
+			else{//when try section executes and user doesn't wish to re-enter
+				return true; 
+			}
 	}// end of displayShowtimes
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	/* sets selectedShowtime attribute by taking input of CinemaType, Date, and Time
 	 * as viewed from the displayShowtimes function
+	 * Returns Showtime object which is null in case of unsuccessful 
 	 */
-	public void selectShowtime() {
+	public Showtime selectShowtime() {
 		IShowtime showtimeHandler = ShowtimeManager.getInstance();
 		
+		//A) CinemaType
+		boolean cinemaTypeScanContd = true;
 		do {
-			//A) CinemaType
-			boolean cinemaTypeScanContd = true;
-			do {
-				System.out.print("Cinema Class, from 'Platinum', 'Gold', 'Sliver' : "); 
-					//display header is class but attribute is CinemaType
-					//Silver misspelled as Sliver in enum definition - UPDATE if changed
-				selectedCinemaTypeName = scan.next();
-				System.out.println();
-						
-				switch(selectedCinemaTypeName) {
-				case "Platinum": 
-					selectedCinemaType = CinemaType.PLATINUM;
-					cinemaTypeScanContd = false;
-					break;
-				case "Gold": 
-					selectedCinemaType = CinemaType.GOLD;
-					cinemaTypeScanContd = false;
-					break;
-				case "Silver": 
-					selectedCinemaType = CinemaType.SILVER; //update to SILVER in CinemaType
-					cinemaTypeScanContd = false;
-					break;
-				default:
-					System.out.println("Erroneous value entered, please try again");
-					break;
-				}
-				}while(cinemaTypeScanContd);//endCinemaScan 		
-				
-			//B) Date - prompt until length 8
-			do {
-				System.out.print("Date (length 8): ");
-				selectedDate = scan.next();
-			}while(selectedDate.length() != 8); 
+			System.out.print("Cinema Class, from 'Platinum', 'Gold', 'Sliver' : ");
+			selectedCinemaTypeName = scan.next();
 			System.out.println();
-			
-			//convert string date to required format for Day constructor
-			int dateInt = 0, dayNumber = 0, monthNumber = 0,yearNumber = 0;
-			try {
-				dateInt = Integer.parseInt(selectedDate);
+					
+			switch(selectedCinemaTypeName) {
+			case "Platinum": 
+				selectedCinemaType = CinemaType.PLATINUM;
+				cinemaTypeScanContd = false;
+				break;
+			case "Gold": 
+				selectedCinemaType = CinemaType.GOLD;
+				cinemaTypeScanContd = false;
+				break;
+			case "Silver": 
+				selectedCinemaType = CinemaType.Silver;
+				cinemaTypeScanContd = false;
+				break;
+			default:
+				System.out.println("Erroneous value entered, please try again");
+				break;
 			}
-			catch (NumberFormatException edate){
-		        edate.printStackTrace();
-		        continue;
-		    }	
+			}while(cinemaTypeScanContd);//endCinemaScan 		
 			
-			// reversing fullDate = Integer.toString(this.yearNumber) + monthString + dayString;
-			dayNumber = dateInt%100;
-			dateInt/=100;
-			monthNumber = dateInt%100;
-			dateInt/=100;
-			yearNumber = dateInt;		
-							
-			//C) Time - implement input check if needed
-			System.out.print("Time: ");
-			selectedTime = scan.next();
-			System.out.println();	
-				
-				//CHECK for bugs
-				try {			
-				selectedDay = new Day(dayNumber, monthNumber, yearNumber, selectedTime);
-				}
-				catch(IllegalArgumentException eday) {
-					System.out.println("Invalid date string supplied");
-					continue; 
-				}
-							
-				try {
-					//add getShowtime to IShowtime interface
-					selectedShowtime = showtimeHandler.getShowtime(selectedMovieName, selectedDay, selectedCineplexID, selectedCinemaType);
-					showtimeID =  selectedShowtime.getID();
-				}
-				catch(IllegalArgumentException e) {
-					System.out.println("Showtime is not found for details entered");
-					System.out.println();
-				}
-		}while(selectedShowtime == null);
+		//B) Date - prompt until length 8
+		do {
+			System.out.print("Date (length 8): ");
+			selectedDate = scan.next();
+		}while(selectedDate.length() != 8); 
+		System.out.println();
 		
-		ShowtimeManager.close();
+		//convert string date to required format for Day constructor
+		int dateInt = 0, dayNumber = 0, monthNumber = 0,yearNumber = 0;
+		try {
+			dateInt = Integer.parseInt(selectedDate);
+		}
+		catch (NumberFormatException edate){
+	        edate.printStackTrace();
+	    }	
+		
+		// reversing fullDate = Integer.toString(this.yearNumber) + monthString + dayString;
+		dayNumber = dateInt%100;
+		dateInt/=100;
+		monthNumber = dateInt%100;
+		dateInt/=100;
+		yearNumber = dateInt;		
+						
+		//C) Time - implement input check if needed
+		System.out.print("Time: ");
+		selectedTime = scan.nextLine();
+		System.out.println();	
+			
+		//CHECK for bugs
+		try {			
+		selectedDay = new Day(dayNumber, monthNumber, yearNumber, selectedTime);
+			try {
+				//add getShowtime to IShowtime interface
+				selectedShowtime = showtimeHandler.getShowtime(selectedMovieName, selectedDay, selectedCineplexID, selectedCinemaType);
+				showtimeID =  selectedShowtime.getID();
+			}
+			catch(IllegalArgumentException e) {
+				System.out.println("Showtime is not found for details entered\n");
+			}
+		}
+		catch(IllegalArgumentException eday) {
+			System.out.println("Invalid date string supplied");
+		}
+					
+		return selectedShowtime;
 	}
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////
@@ -244,5 +223,5 @@ public class CustomerShowtime {
 		seatCol = scan.nextInt();
 		System.out.println();
 	}
-	
 }
+
